fb4e7cdf952ea154ae5a6f2f455548391bffa0ae
Himanshu Chaudhary <78428938+himughanakash@users.noreply.github.com>
2023-09-14 17:24:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
 import About from './components/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
-
-  <Router>
-    <Routes>
-      <Route path="/about" element={<About/>}>
-
-      </Route>
-    </Routes>
-  </Router>
 
   const showAlert = (message, type)=>{
     setAlert({
@@ -50,15 +38,20 @@ function App() {
     <>
       {/*  <Navbar title="TextEditor" aboutText="About Us" /> */}
       {/* <Navbar/> */}
+      <BrowserRouter>
       <Navbar title="TextEditor" mode ={mode} toggleMode = {toggleMode} />
       <Alert alert={alert}/> 
       <div className = "container my-3">
-      <TextForm showAlert = {showAlert} heading = "Enter the text to convert" mode = {mode}/>
-      {/* <About/> */}
+      <Routes>
+        <Route path="/about" element={<About/>}>
+        </Route>
+        <Route path="/" element={<TextForm showAlert = {showAlert} heading = "Enter the text to convert" mode = {mode}/>}>
+        </Route>
+      </Routes>
       </div>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
