1075cc50c45879991602e228e7895e41ccf3fd7f
Himanshu Chaudhary <78428938+himughanakash@users.noreply.github.com>
2023-09-15 17:03:17 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,72 +1,47 @@
-import React, { useState } from 'react'
-import Navbar from './Navbar'
-export default function About() {
-
-const[myStyle, setMyStyle] = useState({
-  color:'black',
-  backgroundColor: 'white'
-})
-const [btntext, setBtnText] = useState("DarkMode")
-
-const toggleStyle = ()=>{
-  if(myStyle.color === 'black'){
-    setMyStyle({
-      color : 'white',
-      backgroundColor: 'black',
-      border: '1px solid white'
-    })
-    setBtnText("LightMode")
-  }
-  else{
-    setMyStyle({
-      color: 'black',
-      backgroundColor:'white'
-    })
-   // setBtn("DarkMode");
-  }
-}
+import React from 'react'
+export default function About(props) {
 
   return (
-    <div className = "container" style={myStyle}>
-    <h1 className="my-3">About Us</h1>
-    <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id='headingOne'>
-      <button class="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body" style={myStyle}>
-        <strong>This is the first item's body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div className = "container">
+      <h1 className="my-3" id="aboutt" style={{color: props.mode === 'dark'?'white':'#042743'}}> About Us</h1>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id='headingOne'>
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <strong>Description</strong> 
+            </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+          TEXTEDITOR is a versatile text manipulation and formatting utility developed using cutting-edge web technologies. This innovative tool empowers users to effortlessly transform their text in various ways, enhancing their writing and editing experience. Whether you need to convert text to uppercase, lowercase, or simply copy it, TEXTEDITOR has you covered.
+        </div>
       </div>
     </div>
-  </div>
-  <div class="accordion-item" style={myStyle}>
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body" style={myStyle}>
-        <strong>This is the second item's body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div className="accordion-item">
+      <h2 className="accordion-header"  id='headingTwo'>
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+         <strong>Functionality</strong>
+        </button>
+      </h2>
+      <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+        TEXTEDITOR provides an intuitive interface that allows users to convert text to uppercase or lowercase with just a click. This feature simplifies the task of formatting text to match specific requirements.
+        </div>
       </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body" style={myStyle}>
-        <strong>This is the third item's body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div className="accordion-item">
+      <h2 className="accordion-header"  id='headingThree'>
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+           <strong>Responsiveness</strong>
+        </button>
+      </h2>
+      <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+        TEXTEDITOR has been meticulously designed with responsiveness in mind. Its flexible layout adapts seamlessly to various devices, ensuring a consistent and user-friendly experience whether you're using it on a desktop, tablet, or smartphone.
+        </div>
       </div>
     </div>
-  </div>
-</div>
     </div>
+  </div>
   )
 }
